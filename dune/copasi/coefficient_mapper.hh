9d896a1665bb4eea0f82d77ae5ea77f9d7fbe853
Santiago Ospina De Los Rios <santiago.ospina@iwr.uni-heidelberg.de>
2019-09-06 16:31:55 +0200

    Split coefficient mapper for simple model and multidomain model

--- dune/copasi/coefficient_mapper.hh
+++ dune/copasi/coefficient_mapper.hh
@@ -1,17 +1,23 @@
 #ifndef DUNE_COPASI_GLOBAL_COEFFICIENT_MAPPER_HH
 #define DUNE_COPASI_GLOBAL_COEFFICIENT_MAPPER_HH
 
+#include <dune/copasi/concepts/grid.hh>
+
 #include <dune/pdelab/gridfunctionspace/lfsindexcache.hh>
 #include <dune/pdelab/gridfunctionspace/localfunctionspace.hh>
 
-#include <unordered_map>
-#include <vector>
+#include <map>
 #include <memory>
+#include <vector>
 
 namespace Dune::Copasi {
 
 struct DefaultCoefficientMapper
 {
+  DefaultCoefficientMapper(const ParameterTree& operator_config,
+                           std::size_t this_op)
+  {}
+
   template<class XViewLocal>
   auto operator()(const XViewLocal& x_view_local,
                   const std::size_t& comp,
@@ -32,10 +38,8 @@ struct DefaultCoefficientMapper
 };
 
 template<class ModelState>
-struct ModelCoefficientMapper
+struct ModelCoefficientMapperBase
 {
-  // component -> (operator,child)
-  std::unordered_map<std::size_t, std::array<std::size_t, 2>> _mapper;
   const std::size_t _this_op;
 
   using GFS = typename ModelState::GridFunctionSpace;
@@ -47,38 +51,102 @@ struct ModelCoefficientMapper
   using SolutionVector =
     Dune::PDELab::LocalVector<SolutionElement, Dune::PDELab::TrialSpaceTag>;
 
-  std::map<std::size_t,std::shared_ptr<LFS>> _lfs;
-  std::map<std::size_t,std::shared_ptr<LFSCache>> _lfs_cache;
-  std::map<std::size_t,std::shared_ptr<XView>> _x_view;
-  std::map<std::size_t,std::shared_ptr<SolutionVector>> _x;
+  std::map<std::size_t, std::shared_ptr<LFS>> _lfs;
+  std::map<std::size_t, std::shared_ptr<LFSCache>> _lfs_cache;
+  std::map<std::size_t, std::shared_ptr<XView>> _x_view;
+  std::map<std::size_t, std::shared_ptr<SolutionVector>> _x;
 
-  ModelCoefficientMapper(const std::vector<std::size_t>& map_operator,
-                         std::size_t this_op)
+  ModelCoefficientMapperBase(std::size_t this_op)
     : _this_op(this_op)
-  {
-    std::vector<std::size_t> comp_child(map_operator.size(), 0);
+  {}
 
-    for (std::size_t i = 0; i < map_operator.size(); i++)
-      _mapper[i] = std::array<std::size_t, 2>{ map_operator[i],
-                                               comp_child[map_operator[i]]++ };
-  }
-
-  void update(const std::map<std::size_t,ModelState>& states)
+  void update(const std::map<std::size_t, ModelState>& states)
   {
+
     _x.clear();
     _x_view.clear();
     _lfs_cache.clear();
     _lfs.clear();
 
-    for (std::size_t i = 0; i < states.size(); i++) {
-      if (_this_op == i)
+    for (const auto& [op, state] : states) {
+      _lfs[op] = std::make_shared<LFS>(state.grid_function_space);
+      if (_this_op == op)
         continue;
-      _lfs[i] = std::make_shared<LFS>(states.at(i).grid_function_space);
-      _lfs_cache[i] = std::make_shared<LFSCache>(*_lfs[i]);
-      _x_view[i] = std::make_shared<XView>(*(states.at(i).coefficients));
-      auto max_local_size = states.at(i).grid_function_space->maxLocalSize();
-      _x[i] = std::make_shared<SolutionVector>(max_local_size);
+      _lfs_cache[op] = std::make_shared<LFSCache>(*_lfs[op]);
+      _x_view[op] = std::make_shared<XView>(*(state.coefficients));
+      auto max_local_size = state.grid_function_space->maxLocalSize();
+      _x[op] = std::make_shared<SolutionVector>(max_local_size);
     }
+  }
+
+  template<class E>
+  void bind(const E& entity)
+  {
+    for (const auto& [op, xx] : _lfs) {
+      if (_this_op == op)
+        continue;
+      _lfs[op]->bind(entity);
+      _lfs_cache[op]->update();
+      _x[op]->resize(_lfs_cache[op]->size());
+      _x_view[op]->bind(*_lfs_cache[op]);
+      _x_view[op]->read(*_x[op]);
+    }
+  }
+
+  void unbind()
+  {
+    for (auto& [op, x_view] : _x_view)
+      x_view.unbind();
+  }
+};
+
+template<class ModelState>
+struct ModelCoefficientMapper : public ModelCoefficientMapperBase<ModelState>
+{
+  using Base = ModelCoefficientMapperBase<ModelState>;
+  using Base::_lfs;
+  using Base::_this_op;
+  using Base::_x;
+
+  // component -> (operator,child)
+  std::unordered_map<std::size_t, std::array<std::size_t, 2>> _comp_mapper;
+
+  ModelCoefficientMapper(std::size_t this_op = 0)
+    : Base(this_op)
+  {}
+
+  ModelCoefficientMapper(const std::vector<std::size_t>& map_operator,
+                         std::size_t this_op)
+    : Base(this_op)
+  {
+    set_mapper(map_operator);
+  }
+
+  ModelCoefficientMapper(const ParameterTree& operator_config,
+                         std::size_t this_op)
+    : Base(this_op)
+  {
+    // map component -> operator
+    std::vector<std::size_t> map_op;
+    auto comp_names = operator_config.getValueKeys();
+    std::sort(comp_names.begin(), comp_names.end());
+
+    for (std::size_t j = 0; j < comp_names.size(); j++) {
+      std::size_t k = operator_config.template get<std::size_t>(comp_names[j]);
+      map_op.push_back(k);
+    }
+
+    set_mapper(map_op);
+  }
+
+  void set_mapper(const std::vector<std::size_t>& map_op)
+  {
+    std::size_t max_op = *std::max_element(map_op.begin(), map_op.end());
+    std::vector<std::size_t> comp_child(max_op + 1, 0);
+
+    for (std::size_t i = 0; i < map_op.size(); i++)
+      _comp_mapper[i] =
+        std::array<std::size_t, 2>{ map_op[i], comp_child[map_op[i]]++ };
   }
 
   template<class XViewLocal>
@@ -86,13 +154,41 @@ struct ModelCoefficientMapper
                   const std::size_t& comp,
                   const std::size_t& dof) const
   {
-    const auto& map = _mapper.find(comp)->second;
+    const auto& map = _comp_mapper.find(comp)->second;
     if (map[0] == _this_op)
       return x_view_local(map[1], dof);
-    else
-    {
+    else {
       const auto& x_view_global = *_x.at(map[0]);
       const auto& lfs = _lfs.at(map[0])->child(map[1]);
+      return x_view_global(lfs, dof);
+    }
+  }
+};
+
+template<class ModelState>
+struct MultiDomainModelCoefficientMapper
+  : public ModelCoefficientMapper<ModelState>
+{
+  using Base = ModelCoefficientMapper<ModelState>;
+  using Base::_comp_mapper;
+  using Base::_lfs;
+  using Base::_this_op;
+  using Base::_x;
+  using Base::Base;
+  std::size_t _domain;
+
+  template<class XViewLocal>
+  auto operator()(const XViewLocal& x_view_local,
+                  const std::size_t& comp,
+                  const std::size_t& dof) const
+  {
+    const auto& map = _comp_mapper.find(comp)->second;
+    if (map[0] == _this_op)
+      return x_view_local(map[1], dof);
+    else {
+      const auto& x_view_global = *_x.at(map[0]);
+      const auto& lfs = _lfs.at(map[0])->child(_domain).child(map[1]);
+      assert(lfs.size() > 0);
       return x_view_global(lfs, dof);
     }
   }
@@ -100,21 +196,15 @@ struct ModelCoefficientMapper
   template<class E>
   void bind(const E& entity)
   {
-    for (std::size_t i = 0; i < _lfs.size(); i++) {
-      if (_this_op == i)
-        continue;
-      _lfs[i]->bind(entity);
-      _lfs_cache[i]->update();
-      _x[i]->resize(_lfs_cache[i]->size());
-      _x_view[i]->bind(*_lfs_cache[i]);
-      _x_view[i]->read(*_x[i]);
-    }
-  }
-
-  void unbind()
-  {
-    for (std::size_t i = 0; i < _lfs.size(); i++)
-      _x_view[i].unbind();
+    Base::bind(entity);
+    const auto gv = _lfs[_this_op]->gridFunctionSpace().gridView();
+    using Grid = std::decay_t<decltype(gv.grid())>;
+    static_assert(Concept::isMultiDomainGrid<Grid>(),
+                  "MultiDomainModelCoefficientMapper should only be used with "
+                  "non-ovelapping multidomain grids");
+    auto domain_set = gv.indexSet().subDomains(entity);
+    assert(domain_set.size() == 1);
+    _domain = *(domain_set.begin());
   }
 };
 
