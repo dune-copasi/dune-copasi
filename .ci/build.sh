d13fff49e5e36bd7357dd9e61fff751242b61d7f
Santiago Ospina De Los Rios <sospinar@gmail.com>
2020-02-11 22:41:58 +0100

    Remove symlinks on for windows CI

--- .ci/build.sh
+++ .ci/build.sh
@@ -15,4 +15,28 @@ g++ --version
 gcc --version
 cmake --version
 
+# on windows, symlinks from git repos don't work
+# msys git replaces symlinks with a text file containing the linked file location
+# so here we identify all such files, and replace them with the linked file
+# note msys defines MSYSTEM variable: use this to check if we are on msys/windows
+if [[ $MSYSTEM ]]; then
+	rootdir=$(pwd)
+		for repo in $(ls -d dune-*/)
+		do
+			echo "repo: $repo"
+			cd $rootdir/$repo
+			for f in $(git ls-files -s | awk '/120000/{print $4}')
+			do
+				dname=$(dirname "$f")
+				fname=$(basename "$f")
+				relf=$(cat $f)
+				src="$rootdir/$repo/$dname/$relf"
+				dst="$rootdir/$repo/$dname/$fname"
+				echo "  - copying $src --> $dst"
+				cp $src $dst
+			done
+		done
+	cd $rootdir
+fi
+
 ${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --only=dune-copasi all
