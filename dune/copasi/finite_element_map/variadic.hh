09d5d745612c13cf58a49ab5f3b95e63ff2d519a
Santiago Ospina De Los Rios <sospinar@gmail.com>
2020-02-11 18:37:10 +0100

    Fix constructor with references and document class

--- dune/copasi/finite_element_map/variadic.hh
+++ dune/copasi/finite_element_map/variadic.hh
@@ -14,16 +14,45 @@
 
 namespace Dune::Copasi {
 
+/**
+ * @brief      This class describes a variadic local finite element map.
+ * @details    The idea is that this class models a local finite element map
+ *             and returns one of the variadic types depending on the requested 
+ *             entity. For doing so, this class virtualize every argument of the 
+ *             variadic local finite element map and returns a (polymorphic) 
+ *             reference of the base interface depending on the entity mapper.
+ *             The entity mapper is a function that receives an entity and returns an
+ *             index that correspond to the i-th variadic local finite element map.
+ *             For example, this class could be used to return Pk finite elements on simplices
+ *             and Qk for cubes. Another example would be to return P0 on cubes and Pk on simplices.
+ *
+ * @tparam     Entity                  Entity type 
+ * @tparam     LocalFiniteElementMaps  Local finite element maps
+ */
 template<class Entity, class... LocalFiniteElementMaps>
 class VariadicLocalFiniteElementMap
 {
+  //! Base interface to return
   using Interface = std::common_type_t<typename VirtualLocalFiniteElementMapWrapper<LocalFiniteElementMaps, Entity>::Interface...>;
+
+  //! Number of local finite elements
   static constexpr std::size_t _size = sizeof...(LocalFiniteElementMaps);
+  
+  //! size for static loops
   using _integral_size = std::integral_constant<std::size_t,_size>;
 
+  /**
+   * @brief      Makes an array of virtualized finite element map pointers from a tuple of finite element maps.
+   *
+   * @param      finite_element_maps  Tuple of unique pointers to finite element maps
+   *
+   * @tparam     FEMs                 Finite element map types
+   *
+   * @return     Array of unique pointers to virtualized finite element maps
+   */
   template<class... FEMs>
   static std::array<std::unique_ptr<const Interface>,_size>
-  make_unique_array_from_tuple(std::tuple<std::unique_ptr<const FEMs>...>&& finite_element_maps)
+  make_unique_array_from_tuple(std::tuple<std::unique_ptr<FEMs>...>&& finite_element_maps)
   {
     using VariadicFEMs = std::tuple<std::decay_t<LocalFiniteElementMaps>...>;
     std::array<std::unique_ptr<const Interface>,_size>  fem_unique_array;
@@ -39,51 +68,106 @@ class VariadicLocalFiniteElementMap
     return fem_unique_array;
   }
 
-  // template<class... FEMs>
-  // static std::array<std::unique_ptr<const Interface>,_size>
-  // make_unique_array_from_variadic(FEMs&&... finite_element_maps)
-  // {
-  //   return make_unique_array_from_tuple(std::forward_as_tuple(std::make_unique<const std::decay_t<FEMs>>(std::forward<FEMs>(finite_element_maps))...));
-  // }
+  /**
+   * @brief      Makes an array of virtualized finite element map pointers from finite element maps.
+   *
+   * @param      finite_element_maps  The finite element maps
+   *
+   * @tparam     FEMs                 The finite element map types
+   *
+   * @return     Array of unique pointers to virtualized finite element maps
+   */
+  template<class... FEMs>
+  static std::array<std::unique_ptr<const Interface>,_size>
+  make_unique_array_from_variadic(FEMs&&... finite_element_maps)
+  {
+    auto tuple = std::make_tuple(std::make_unique<std::decay_t<FEMs>>(std::forward<FEMs>(finite_element_maps))...);
+    return make_unique_array_from_tuple(std::move(tuple));
+  }
 
 public:
   using Traits = typename Interface::Traits;
 
-  // if this fails, finite element maps do not have common dimension!
+  //! dimension of finite element map
   static constexpr int dimension = std::common_type_t<std::integral_constant<int,LocalFiniteElementMaps::dimension>...>::value;
 
+  /**
+   * @brief      Constructs a new instance.
+   *
+   * @param[in]  entity_mapper        The entity mapper
+   * @param      finite_element_maps  Array of virtualized finite element maps unique pointers
+   *
+   * @tparam     EntityMapper         A functor which when evaluated with an entity returns an (std::size_t) index
+   */
   template<class EntityMapper>
   explicit VariadicLocalFiniteElementMap(const EntityMapper& entity_mapper, std::array<std::unique_ptr<const Interface>,_size>&& finite_element_maps)
     : _entity_mapper(entity_mapper)
     , _finite_element_maps(std::move(finite_element_maps))
   {}
-
+  
+  /**
+   * @brief      Constructs a new instance.
+   *
+   * @param[in]  entity_mapper        The entity mapper
+   * @param      finite_element_maps  Tuple of finite element maps unique pointers
+   *
+   * @tparam     EntityMapper         A functor which when evaluated with an entity returns an (std::size_t) index
+   */
   template<class EntityMapper>
-  explicit VariadicLocalFiniteElementMap(const EntityMapper& entity_mapper, std::tuple<std::unique_ptr<const LocalFiniteElementMaps>...>&& finite_element_maps)
+  explicit VariadicLocalFiniteElementMap(const EntityMapper& entity_mapper, std::tuple<std::unique_ptr<LocalFiniteElementMaps>...>&& finite_element_maps)
     : VariadicLocalFiniteElementMap(entity_mapper, make_unique_array_from_tuple(std::move(finite_element_maps)))
   {}
 
-  // template<class EntityMapper, class... FEMs>
-  // explicit VariadicLocalFiniteElementMap(const EntityMapper& entity_mapper, FEMs&&... finite_element_maps)
-  //   : VariadicLocalFiniteElementMap(entity_mapper, make_unique_array_from_variadic(std::forward<FEMs>(finite_element_maps)...))
-  // {}
-
-  ~VariadicLocalFiniteElementMap()
+  /**
+   * @brief      Constructs a new instance.
+   *
+   * @param[in]  entity_mapper        The entity mapper
+   * @param      finite_element_maps  The finite element maps
+   *
+   * @tparam     EntityMapper         A functor which when evaluated with an entity returns an (std::size_t) index
+   * @tparam     FEMs                 Finite element map types
+   */
+  template<class EntityMapper, class... FEMs>
+  explicit VariadicLocalFiniteElementMap(const EntityMapper& entity_mapper, FEMs&&... finite_element_maps)
+    : VariadicLocalFiniteElementMap(entity_mapper, make_unique_array_from_variadic(std::forward<FEMs>(finite_element_maps)...))
   {}
 
+  //! Default destructor
+  ~VariadicLocalFiniteElementMap() = default;
+
+  /**
+   * @brief      Searches for a finite element for entity e
+   *
+   * @param[in]  e     Entity
+   *
+   * @return     A finite element for i-th finite element map according to the entity mapper
+   */
   const typename Traits::FiniteElementType&
   find (const typename Traits::EntityType& e) const
   {
-    assert(_entity_mapper(e) < _size);
-    assert(_entity_mapper(e) >= 0);
-    return _finite_element_maps[_entity_mapper(e)]->find(e);
+    std::size_t index = _entity_mapper(e);
+    assert(index < _size);
+    assert(index >= 0);
+    return _finite_element_maps[index]->find(e);
   }
 
+  /**
+   * @brief      Fixed size of the degrees of freedom wrt entities
+   *
+   * @return     False if there is more than one varaidic fem, otherwise theresult of the wrapped fem
+   */
   bool fixedSize() const
   {
     return _size == 1 ? _finite_element_maps[0]->fixedSize() : false;
   }
 
+  /**
+   * @brief      Size for the geomertry type gt
+   *
+   * @param[in]  gt    Geometry type
+   *
+   * @return     The size if there is only one variadic fem, otherwise throw
+   */
   std::size_t size(GeometryType gt) const
   {
     if constexpr (_size == 1)
@@ -93,6 +177,13 @@ public:
         "this method should not be called for not fixed sizes");
   }
 
+  /**
+   * @brief      Determines if codim has degrees of freedom
+   *
+   * @param[in]  codim  The codim
+   *
+   * @return     true if codim has degrees of freedom for any variadic fem
+   */
   bool hasDOFs(int codim) const
   {
     bool has_dofs = false;
@@ -101,6 +192,11 @@ public:
     return has_dofs;
   }
 
+  /**
+   * @brief      Max local size for this finite element
+   *
+   * @return     Max local size of the variadic maxLocalSizes
+   */
   std::size_t maxLocalSize() const
   {
     std::size_t max_local_size(0);
@@ -144,7 +240,7 @@ struct Factory<VariadicLocalFiniteElementMap<Entity,LocalFiniteElementMaps...>>
     static_assert(dCtx::has( Context::Tag<EntityMapper>{} ), "Context does not contain a EntityMapper");
 
     return std::make_unique<FEM>(ctx.view( Context::Tag<EntityMapper>{} ),
-                                 std::forward_as_tuple(Factory<LocalFiniteElementMaps>::create(ctx)...));
+                                 std::make_tuple(Factory<LocalFiniteElementMaps>::create(ctx)...));
   }
 };
 
