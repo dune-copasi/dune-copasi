04662e63386dae2cdf1e62cf78682347059aedb5
Santiago Ospina De Los Rios <sospinar@gmail.com>
2020-02-11 23:57:16 +0100

    Add more info to CI to diagnose Appevyor problem

--- .ci/build.sh
+++ .ci/build.sh
@@ -8,6 +8,7 @@ echo "DUNECONTROL: ${DUNECONTROL}"
 echo "DUNE_OPTIONS_FILE: ${DUNE_OPTIONS_FILE}"
 cat ${DUNE_OPTIONS_FILE}
 echo "PWD: $PWD"
+git -C dune-copasi pull
 ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
 
 which g++
@@ -16,29 +17,5 @@ which cmake
 g++ --version
 gcc --version
 cmake --version
-
-# on windows, symlinks from git repos don't work
-# msys git replaces symlinks with a text file containing the linked file location
-# so here we identify all such files, and replace them with the linked file
-# note msys defines MSYSTEM variable: use this to check if we are on msys/windows
-if [[ $MSYSTEM ]]; then
-	rootdir=$(pwd)
-		for repo in $(ls -d dune-copasi)
-		do
-			echo "repo: $repo"
-			cd $rootdir/$repo
-			for f in $(git ls-files -s | awk '/120000/{print $4}')
-			do
-				dname=$(dirname "$f")
-				fname=$(basename "$f")
-				relf=$(cat $f)
-				src="$rootdir/$repo/$dname/$relf"
-				dst="$rootdir/$repo/$dname/$fname"
-				echo "  - copying $src --> $dst"
-				cp $src $dst
-			done
-		done
-	cd $rootdir
-fi
 
 ${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --only=dune-copasi all
