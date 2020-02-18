915f7efdc63c611c5c2ab600e200b6cd036ee723
Santiago Ospina De Los Rios <sospinar@gmail.com>
2020-02-18 14:37:39 +0100

    Document grid utilities

--- dune/copasi/grid/has_single_geometry_type.hh
+++ dune/copasi/grid/has_single_geometry_type.hh
@@ -7,6 +7,18 @@
 
 namespace Dune::Copasi {
 
+/**
+ * @brief      Determines if grid view has a single geometry type.
+ * @details    If the grid only knows about one geometry type at compile-time,
+ *             this function is constexpr and always true. Notice however,
+ *             that the countrary does not hold and the check is done at run-time.
+ *
+ * @param[in]  grid_view  The grid view
+ *
+ * @tparam     GridView   The grid view type
+ *
+ * @return     True if single geometry type, False otherwise.
+ */
 template<class GridView>
 std::enable_if_t<Capabilities::hasSingleGeometryType<typename GridView::Grid>::v,bool>
 constexpr has_single_geometry_type(const GridView&)
@@ -14,6 +26,19 @@ constexpr has_single_geometry_type(const GridView&)
   return true;
 }
 
+
+/**
+ * @brief      Determines if grid view has a single geometry type.
+ * @details    If the grid only knows about one geometry type at compile-time,
+ *             this function is constexpr and always true. Notice however,
+ *             that the countrary does not hold and the check is done at run-time.
+ *
+ * @param[in]  grid_view  The grid view
+ *
+ * @tparam     GridView   The grid view type
+ *
+ * @return     True if single geometry type, False otherwise.
+ */
 template<class GridView>
 std::enable_if_t<not Capabilities::hasSingleGeometryType<typename GridView::Grid>::v,bool>
 has_single_geometry_type(const GridView& grid_view)
@@ -21,10 +46,7 @@ has_single_geometry_type(const GridView& grid_view)
   GeometryType gt = grid_view.template begin<0>()->geometry().type();
   for (auto&& element : elements(grid_view))
     if (gt != element.geometry().type())
-    {
-      std::cout << element.geometry().type() << std::endl;
       return false;
-    }
   return true;
 }
 
