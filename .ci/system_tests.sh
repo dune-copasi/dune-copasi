d5f9c6d96b510c91a6b9ec6768ecf9f7932c3ef6
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-01-27 18:49:48 +0100

    Fix test scripts to take into account CMAKE_FLAGS from options file

--- .ci/system_tests.sh
+++ .ci/system_tests.sh
@@ -8,6 +8,7 @@ set -e
 PATH=/mingw64/bin:$PATH
 echo "PATH=$PATH"
 echo "MSYSTEM: $MSYSTEM"
+echo "DUNE_OPTIONS_FILE: ${DUNE_OPTIONS_FILE}"
 echo "PWD: $PWD"
 
 which g++
@@ -17,7 +18,14 @@ g++ --version
 gcc --version
 cmake --version
 
+if test "x$DUNE_OPTS_FILE" != "x"; then
+  CMAKE_FLAGS="$(. $DUNE_OPTS_FILE; eval echo \$CMAKE_FLAGS)"
+fi
+
 mkdir dune-copasi/test/build-cmake && cd dune-copasi/test/build-cmake
-cmake --build ..
+
+echo "cmake $CMAKE_FALGS --build .."
+eval cmake $CMAKE_FALGS --build ..
+
 cmake --target build_system_tests
 ctest -j4 -L "DUNE_SYSTEMTEST" --output-on-failure
