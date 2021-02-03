1a292a598bc66a48b1b990a197bd8021878d23e6
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-02-03 03:48:00 +0100

    Try to revert setup changes
    * I don't understand why cmake complaints about the directory. So let's try to roll back

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -1,49 +1,37 @@
-#!/usr/bin/env bash
-
 # dependencies setup script for Travis and AppVeyor CI
-# requisites
-#   * DUNE_OPTIONS_FILE is defined
-
-set -e
 
 DUNE_VERSION="2.7"
 
 # make sure we get the right mingw64 version of g++ on appveyor
 PATH=/mingw64/bin:$PATH
+echo "PATH=$PATH"
+echo "MSYSTEM: $MSYSTEM"
+echo "DUNECONTROL: ${DUNECONTROL}"
+echo "DUNE_OPTIONS_FILE: ${DUNE_OPTIONS_FILE}"
+cat ${DUNE_OPTIONS_FILE}
+echo "PWD: $PWD"
 
-onfailure() {
-	echo $PWD
-	ls
+which g++
+g++ --version
 
-	echo "PATH=$PATH"
-	echo "MSYSTEM: $MSYSTEM"
-	echo "DUNE_OPTIONS_FILE: ${DUNE_OPTIONS_FILE}"
-	cat ${DUNE_OPTIONS_FILE}
-	echo "PWD: $PWD"
+which gcc
+gcc --version
 
-	which g++
-	g++ --version
+which python
+python --version
 
-	which gcc
-	gcc --version
+which python3
+python3 --version
 
-	which python
-	python --version
+which pip
+pip --version
 
-	which python3
-	python3 --version
+which pip3
+pip3 --version
 
-	which pip
-	pip --version
+which cmake
+cmake --version
 
-	which pip3
-	pip3 --version
-
-	which cmake
-	cmake --version
-}
-
-trap onfailure EXIT
 
 # download Dune dependencies
 for repo in core/dune-common core/dune-geometry core/dune-grid core/dune-istl core/dune-localfunctions staging/dune-functions staging/dune-uggrid
@@ -86,59 +74,25 @@ fi
 
 # patch cmake macro to avoid build failure when fortran compiler not found, e.g. on osx
 cd dune-common
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	wget https://gist.githubusercontent.com/lkeegan/059984b71f8aeb0bbc062e85ad7ee377/raw/e9c7af42c47fe765547e60833a72b5ff1e78123c/cmake-patch.txt
-	echo '' >> cmake-patch.txt
-	git apply cmake-patch.txt
-fi
-
+wget https://gist.githubusercontent.com/lkeegan/059984b71f8aeb0bbc062e85ad7ee377/raw/e9c7af42c47fe765547e60833a72b5ff1e78123c/cmake-patch.txt
+echo '' >> cmake-patch.txt
+git apply cmake-patch.txt
 # another patch for missing header in cmake install list
 git apply ../dune-copasi/.ci/dune-common.patch
 cd ../
-
 
 cd dune-logging
 git apply ../dune-copasi/.ci/dune-logging.patch
 cd ../
 
-# hardcoded *ordered* dependencies!
-MODULES="logging uggrid geometry grid localfunctions istl typetree functions pdelab multidomaingrid "
+ls
+
+# python virtual environment does not work in windows yet
 if [[ ! $MSYSTEM ]]; then
-	MODULES+="testtools"
+	${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --module=dune-testtools all
 fi
 
-./dune-common/bin/dunecontrol --opts=${DUNE_OPTIONS_FILE} --only=dune-common all
-cmake --build dune-common/build-cmake/ --target install
-rm -rf dune-common
-
-# get cmake flags
-CMAKE_FLAGS=
-if test "x$DUNE_OPTIONS_FILE" != "x"; then
-  CMAKE_FLAGS="$(. $DUNE_OPTIONS_FILE; eval echo \$CMAKE_FLAGS)"
-fi
-
-# get install prefix
-for flag in $CMAKE_FLAGS; do
-  [[ ${flag#-D} == CMAKE_INSTALL_PREFIX* ]] && CMAKE_INSTALL_PREFIX="${flag#-DCMAKE_INSTALL_PREFIX:PATH=}"
-done
-
-# define DUNECONTROL path
-if test "x$DUNECONTROL" == "x"; then
-	if test "x$CMAKE_INSTALL_PREFIX" != "x"; then
-  	DUNECONTROL="$CMAKE_INSTALL_PREFIX/bin/dunecontrol"
-	else
-		DUNECONTROL=dunecontrol
-	fi
-fi
-
-if ! command -v $DUNECONTROL &> /dev/null; then
-	echo "ERROR: '$DUNECONTROL' cannot be executed"
-	exit 1
-fi
-
-for module in $MODULES
+for repo in dune-testtools dune-logging dune-pdelab dune-multidomaingrid
 do
-	${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --only=dune-$module all
-	cmake --build dune-$module/build-cmake/ --target install
-	rm -rf dune-$module
+  ${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --module=$repo all
 done
