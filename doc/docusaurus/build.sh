88de0200ce82b45c96f3656ca1f57ee728046ee6
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2021-02-08 16:33:29 +0100

    Let's not forget to checkout branches

--- doc/docusaurus/build.sh
+++ doc/docusaurus/build.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
+# Loop over every tag and version documentation if available
+
+set -e
+
 yarn install
 
-set +e
-
 BRANCH=$(cat ../../.git/HEAD | awk -F '/' '{print $NF}')
+git stash
 
 for tag in $(git tag); do
+  git checkout $tag
   if [[ $(git ls-files | grep docs) ]]; then
     yarn run docusaurus docs:version $tag
   fi
 done
 
 git checkout $BRANCH
+git stash pop
 
 yarn build
