0b7671279a804978f91b2d3181a2d53543f7c954
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2021-02-08 16:21:49 +0100

    Change comparison syntax

--- doc/docusaurus/build.sh
+++ doc/docusaurus/build.sh
@@ -6,7 +6,7 @@ set +e
 
 BRANCH=$(cat ../../.git/HEAD | awk -F '/' '{print $NF}')
 
-for tag in $(git tag)
+for tag in $(git tag); do
   if [[ $(git ls-files | grep docs) ]]; then
     yarn run docusaurus docs:version $tag
   fi
