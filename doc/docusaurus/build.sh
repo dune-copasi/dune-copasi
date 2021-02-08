c61e5c0270272bdf13dad3abf88eb9e1b29cf69e
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2021-02-08 16:19:21 +0100

    Only version documents present in current git tag

--- doc/docusaurus/build.sh
+++ doc/docusaurus/build.sh
@@ -7,7 +7,7 @@ set +e
 BRANCH=$(cat ../../.git/HEAD | awk -F '/' '{print $NF}')
 
 for tag in $(git tag)
-  if [[ -d docs ]]; then
+  if [[ $(git ls-files | grep docs) ]]; then
     yarn run docusaurus docs:version $tag
   fi
 done
