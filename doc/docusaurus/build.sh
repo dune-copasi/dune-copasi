4cc07e1f1dc487ba781fea77eb20c46fda7d149f
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-05-05 16:22:27 +0200

    Build and link doxygen to docusaurus

--- doc/docusaurus/build.sh
+++ doc/docusaurus/build.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+
+# install cmake
+curl -OL https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2-linux-x86_64.tar.gz
+tar -xzf cmake-3.20.2-linux-x86_64.tar.gz
+CMAKE=${PWD}/cmake-3.20.2-linux-x86_64/bin/cmake
+
 yarn install
 
 BRANCH=$(cat ../../.git/HEAD | awk -F '/' '{print $NF}')
@@ -12,16 +18,26 @@ git stash
 for tag in $(git tag); do
   echo "----------Checking out $tag-----------"
   if git checkout $tag -- docs; then
-    echo "----------Inspecting filder $tag-----------"
+    echo "----------Inspecting files $tag-----------"
     if [[ $(git ls-files | grep docs) ]]; then
+      git checkout $tag -- ../../dune
+      mkdir docs/doxygen
+      pushd docs/doxygen
+      ${CMAKE} ../../../doxygen && make
+      popd
       echo "----------Versioning $tag-----------"
       yarn run docusaurus docs:version $tag
+      rm -rf docs/doxygen
     fi
   fi
 done
 
 echo "----------Get back to branch $BRANCH-----------"
 git checkout $BRANCH -- docs
+mkdir docs/doxygen
+pushd docs/doxygen
+${CMAKE} ../../../doxygen && make
+popd
 
 echo "----------Build yarn-----------"
 yarn build
