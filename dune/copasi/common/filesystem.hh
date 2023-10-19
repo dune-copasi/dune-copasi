45d231e78adbe1fa269c9504e99f1ee85a8a3f55
Liam Keegan <liam@keegan.ch>
2023-10-19 09:26:09 +0200

    CMake fixes
    - use `dune-copasi-installed-targets.cmake` filename for exported targets as `dune-copasi-targets.cmake` is already used by `finalize_dune_project`
    - export build tree targets to CMAKE_CURRENT_BINARY_DIR
    - add missing header files
    - remove filesystem fallback as c++20 now required so can assume std::filesystem

--- dune/copasi/common/filesystem.hh
+++ dune/copasi/common/filesystem.hh
@@ -3,20 +3,10 @@
 
 #include <dune-copasi-config.hh>
 
-#if HAVE_GHC_FILESYSTEM
-
-#include <ghc/filesystem.hpp>
-namespace Dune::Copasi {
-namespace fs = ghc::filesystem;
-}
-#else
-
 #include <filesystem>
 
 namespace Dune::Copasi {
 namespace fs = std::filesystem;
 }
-
-#endif
 
 #endif // DUNE_COPASI_FILESYSTEM_HH
