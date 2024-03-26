c0eef8a300f5867e289e858e665c0d690002b53a
Joseph Holten <joseph@holten.com>
2024-03-26 12:11:29 +0100

    Added cd, ls commands

--- doc/docusaurus/src/components/WasmTerminal.js
+++ doc/docusaurus/src/components/WasmTerminal.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ReactTerminal, TerminalContextProvider } from "react-terminal";
+import wasm from "dune-copasi-wasm-git"
 
 export default function WasmTerminal() {
   const theme = "custom";
@@ -12,20 +13,62 @@ export default function WasmTerminal() {
     },
   }
 
-  const [cwd, setCwd] = useState("/dunecopasi")
+  const home = "/dunecopasi"
+  const [cwd, setCwd] = useState(home)
+
+  // setup file system
+  const preRun = (instance) => {
+    instance.FS.mkdir(home)
+    instance.FS.chdir(home)
+  }
+
+  // create wasm instance
+  const [instance, setInstance] = useState(null)
+  useEffect(() => {
+    wasm({preRun}).then(inst => setInstance(inst))
+  }, [])
 
   // Define commands here
   const commands = {
     whoami: "dunecopasi",
-    cd: (args) => {
-      if (args.split(" ").length > 1) 
+    where: () => {
+      return instance.FS.cwd()
+    },
+    cd: (path) => {
+      // guard against multiple paths
+      if (path.split(" ").length > 1) 
         return "Error: usage: cd <path>"
-      const path = args
-      setCwd(path.startsWith("/") ? path : cwd + "/" + path)
-    } 
-     
-  }
 
+      // no argument means home
+      if (path.length === 0)
+        path = startdir
+
+      // switch on absolute vs relative path
+      var newpath = path.startsWith("/") ? path : cwd + "/" + path
+
+      // normalize path to not end with slash
+      if (newpath.endsWith("/") && newpath !== "/")
+        newpath = newpath.slice(0, -1)
+
+      // guard against nonexistent paths
+      if (!instance.FS.analyzePath(newpath).exists)
+        return `Error: ${newpath} is not a valid path`
+
+      setCwd(newpath)
+      instance.FS.chdir(newpath)
+    },
+    ls: (path) => {
+      // guard against multiple paths
+      if (path.split(" ").length > 1) 
+        return "Error: usage: ls <path>"
+
+      // no argument means cwd
+      if (path.length === 0)
+        path = cwd
+
+      return instance.FS.readdir(path).filter((dir) => dir !== "." && dir !== "..").join(" ")
+    },
+  }
 
   return (
     <TerminalContextProvider>
@@ -38,5 +81,5 @@ export default function WasmTerminal() {
         />
       </div>
     </TerminalContextProvider>
-  );
+  )
 }
