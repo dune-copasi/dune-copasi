ec2ac6b1edf1db05080fa2b61f7daf97975bcfbd
Joseph Holten <joseph@holten.com>
2024-03-27 18:43:07 +0100

    Basic readline functionality

--- doc/docusaurus/src/components/WasmXterm.js
+++ doc/docusaurus/src/components/WasmXterm.js
@@ -1,11 +1,6 @@
 import BrowserOnly from '@docusaurus/BrowserOnly'
 import React from "react"
 
-const ASCII = {
-  ENTER: 13,
-  DELETE: 127,
-}
-
 // NOTE: need this extra class to call functions on mount
 //       as we cannot call `useEffect` inside BrowserOnly
 class WasmXtermImpl extends React.Component {
@@ -15,7 +10,8 @@ class WasmXtermImpl extends React.Component {
     this.home = "/dunecopasi"
     this.state = {
       input: "",
-      cwd: this.home
+      cwd: this.home,
+      cursorPos: 0,
     }
   }
 
@@ -30,34 +26,60 @@ class WasmXtermImpl extends React.Component {
   render() {
     // NOTE: cannot import xterm globally as it accesses `document` which fails during SSG
     const XTerm = require("xterm-for-react").XTerm
-    const encoder = new TextEncoder()
+    const write = (t) => this.xtermRef.current.terminal.write(t)
+    const writeln = (t) => this.xtermRef.current.terminal.writeln(t)
+    const newline = () => write("\r\n")
 
-    const onData = (data) => {
-      const bytes = encoder.encode(data)
-      const write = (t) => this.xtermRef.current.terminal.write(t)
+    const prompt = () => write(this.state.cwd + " $ ")
 
-      if (bytes[0] === ASCII.ENTER && this.state.input.length > 0) {
-        write("\r\nYou typed: '" + this.state.input + "'\r\n")
-        write("echo> ")
-        this.setState({ input: "" })
-        return
+    const onKey = (e) => {
+      const printable = !e.altKey && !e.altGraphKey && !e.ctrlKey && !e.metaKey
+      const key = e.domEvent.key
+      console.log(e)
+
+      if (key === "Enter") {
+        newline()
+        writeln(this.state.input)
+        prompt()
+        this.setState({ input: "", cursorPos: 0 })
       }
-      // ignore other control characters
-      else if (bytes[0] < 32) {
-        console.error("WasmXterm: unhandled code", code)
-        return
-      } 
-      else if (bytes[0] === ASCII.DELETE) {
-        if (this.state.input.length > 0) {
-          write("\b \b")
-          this.setState({input: this.state.input.slice(0,-1)})
+      else if (key === "Backspace") {
+        if (this.state.input.length > 0 && this.state.cursorPos > 0) {
+          // move cursor back, override buffer
+          write("\b \b" + this.state.input.slice(this.state.cursorPos-1))
+          // update state
+          this.state((state,props) => { return {
+            input: state.input.slice(0,state.cursorPos-1) + input.slice(state.cursorPos),
+            cursorPos: state.cursorPos-1, 
+          }})
         }
       }
-      // handle regular printable char
-      else if (bytes[0] < 128) {
-        write(data)
-        this.setState({
-          input: this.state.input + data,
+      else if (key === "ArrowLeft") {
+        if (this.state.cursorPos > 0) {
+          // move cursor back
+          write("\b")
+          this.setState((state, props) => {return {
+            cursorPos: state.cursorPos-1,
+          }})
+        }
+      }
+      else if (key === "ArrowRight") {
+        if (this.state.cursorPos < this.state.input.length) {
+          // write what is currently under cursor (moves cursor forward)
+          write(this.state.input[this.state.cursorPos])
+          this.setState((state, props) => {return {
+            cursorPos: state.cursorPos+1,
+          }})
+        }
+      }
+      else if (printable) {
+        this.setState((state,props) => {
+          write(e.key + state.input.slice(state.cursorPos))
+          write("\b".repeat(state.input.length-state.cursorPos))
+          return {
+            input: state.input.slice(0,state.cursorPos) + e.key + state.input.slice(state.cursorPos),
+            cursorPos: state.cursorPos+1,
+          }
         })
       }
     }
@@ -65,7 +87,7 @@ class WasmXtermImpl extends React.Component {
     return (
       <XTerm
         ref={this.xtermRef}
-        onData={onData}
+        onKey={onKey}
       />
     )
   }
